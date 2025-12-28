60b5381fa9aabc3796a7797ef06c34d6c8319b82
Lugen-dl <gena90213@gmail.com>
2025-12-28 21:50:25 +0300

    My future docker project

---
+++ docker_dir/dockerCLI/docker_image_creator.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -eou pipefail
+trap 'echo "Error at line $LINENO"; exit 1' ERR
+
+#Your image function
+source ./image.sh          
+
+
+#Checking the loggin to the Docker hub
+logger () {
+echo "Please log to your docker hub to continue"
+  if ! docker login; then
+    echo "Error, exiting the script"
+      exit 1
+  fi
+    }
+logger
+
+#If succesful, going to the choice menu
+cat <<EOF
+  0. Exit
+  1. Image working
+  2. Container working
+  3. Image building and containerization
+EOF
+    
+read -p "Choose the option > " opt
+#Checking the $opt correctly
+  if [[ -z "$opt" || ! "$opt" =~ ^[0-9]+$ ]]; then
+    echo "Error, wrong format"
+      exit 1
+  fi
+
+#Main Function  
+  case $opt in
+      0) echo "Exiting the script"
+          exit 0
+        ;;
+      1) 
+        image
+        ;;
+      2) 
+esac
+    
+  
---
+++ docker_dir/dockerCLI/image.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+image () {
+  while true; do
+  cat <<EOF
+  1. Search for the official images
+  2. Pull the image or delete
+EOF
+  read -p "Choose > " opt1
+case $opt1 in
+  1)
+    read -p "put the image you want to search " image
+      docker search "$image" --filter is-official=true
+        read -p "Press <enter> to return..."
+            ;;
+  2)
+    read -p "Put the name > " name
+      read -p "Put the tag > " tag
+        full="$name:$tag"
+          read -p "Do you want to pull or delete? [p/d]" answ
+      if [[ "$answ" == "p" ]]; then
+          docker image pull "$full"
+            echo "Your $full was pulled"
+              exit 0
+      elif [[ "$answ" == "d" ]]; then
+          if ! docker container stop "$full"; then
+            echo "Error, cannot remove image with it working container!"
+              exit 1
+            else  
+          docker image rm "$full"
+          fi
+      fi    
+esac
+done
+}
+
